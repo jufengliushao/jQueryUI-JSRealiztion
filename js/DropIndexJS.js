f1edfce8528fe3373192a84b8ffcc239895b78c6
liushaofeng <jufengliushao@163.com>
2017-02-15 14:30:47 +0800

    complete droopable

--- js/DropIndexJS.js
+++ js/DropIndexJS.js
@@ -1,11 +1,26 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/15.
  */
+var inBig = false;
 $(document).ready(function () {
-    $("#small_div").draggable();
+    $("#small_div").draggable({
+
+    });
     $("#big_div").droppable({
         drop: function (event, ui) {
-
+            inBig = true;
+            $("#big_p").html('太热了，你走吧');
+            setPText();
+        },
+        out: function (event, ui) {
+            inBig = false;
+            $("#big_p").html('快到怀里来');
+            setPText();
         }
     });
 });
+
+function setPText() {
+    // document.getElementById("small_p").innerHTML = inBig.toString();
+    $("#small_p").html(inBig.toString());
+}
