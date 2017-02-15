b9873abf772213595e3b310e22f81c359849a1f9
liushaofeng <jufengliushao@163.com>
2017-02-15 21:38:53 +0800

    add selectable

--- js/Select.js
+++ js/Select.js
@@ -1,3 +1,15 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/15.
  */
+$(document).ready(function () {
+    $("#select").selectable({
+        selecting: function (event, ui) {
+            // console.log(ui.selecting.textContent.indexOf("C"));
+            if(ui.selecting.textContent.indexOf("C") == 0){
+                $("#answer").html("恭喜你选对了");
+            }else {
+                $("#answer").html("错啦");
+            }
+        }
+    });
+});
