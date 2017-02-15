046a368eefa9b9277f045858b482d451ef974882
liushaofeng <jufengliushao@163.com>
2017-02-15 17:10:15 +0800

    empty

--- js/Resizable.js
+++ js/Resizable.js
@@ -3,4 +3,29 @@
  */
 $(document).ready(function () {
     $("#jq_div").resizable();
+    document.onmousemove = function (ev) {
+        judgementMouse(ev);
+    };
 });
+
+function judgementMouse(ev) {
+    var jsDiv = document.getElementById("js_div");
+    var left = jsDiv.offsetLeft;
+    var top = jsDiv.offsetTop;
+    var height = jsDiv.clientHeight;
+    var width = jsDiv.clientWidth;
+    console.log(ev);
+    if( ev.offsetY >= top + height - 50 && ev.offsetY <= top + height + 50){
+        jsDiv.className = 'js_sourth';
+        return;
+    }
+    if( ev.offsetX >= left + width - 20 && ev.offsetX <= left + width + 20 && ev.offsetY >= top + height - 20 && ev.offsetY <= top + height + 20){
+        jsDiv.className = 'js_se';
+        return;
+    }
+    if(ev.offsetX >= left + width - 20 && ev.offsetX <= left + width + 20){
+        jsDiv.className = 'js_east';
+        return;
+    }
+    jsDiv.className = 'js_none';
+}
