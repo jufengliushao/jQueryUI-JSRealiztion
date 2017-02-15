0ccb4e3dd852adcdb90a9f8006e48e0b6011e47a
刘少锋 <liushaofeng@cnlive.com>
2017-02-15 08:51:47 +0800

    create project

---
+++ js/DraggableStyle.js
@@ -0,0 +1,6 @@
+/**
+ * Created by cnlive-lsf-doc on 2017/2/14.
+ */
+$(document).ready(function () {
+    $("#jQ_div").draggable();
+});
---
+++ js/indexJS.js
@@ -0,0 +1,8 @@
+/**
+ * Created by cnlive-lsf-doc on 2017/2/14.
+ */
+$(document).ready(function () {
+    $("#a_btn").button(function (event) {
+        alert(event);
+    });
+});
