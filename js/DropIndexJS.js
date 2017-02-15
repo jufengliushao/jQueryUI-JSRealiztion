d0aec6bf7ee123efb6a10c127c6ba51c9c161852
liushaofeng <jufengliushao@163.com>
2017-02-15 11:52:33 +0800

    realize droppable

--- js/DropIndexJS.js
+++ js/DropIndexJS.js
@@ -1,3 +1,11 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/15.
  */
+$(document).ready(function () {
+    $("#small_div").draggable();
+    $("#big_div").droppable({
+        drop: function (event, ui) {
+
+        }
+    });
+});
