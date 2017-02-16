6105653e77795c1116b17ff2b9e386ce680f554e
liushaofeng <jufengliushao@163.com>
2017-02-16 10:11:17 +0800

    add code

--- js/Sortable.js
+++ js/Sortable.js
@@ -1,3 +1,23 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/16.
  */
+$(document).ready(function () {
+    $("#friut").sortable({
+        stop: function (event, ui) {
+            console.log("------stop-------");
+            console.log(ui);
+            console.log("------stop-------");
+        },
+        update: function (event, ui) {
+            console.log("------update-------");
+            console.log(ui);
+            console.log("------update-------");
+        },
+        start: function (event, ui) {
+            console.log("------start-------");
+            console.log(ui);
+            console.log("------start-------");
+        },
+        placeholder: "ui-state-highlight"
+    });
+});
