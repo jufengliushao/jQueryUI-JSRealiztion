32415717d37ca653f8e431bc2be215b421a70976
liushaofeng <jufengliushao@163.com>
2017-02-17 15:14:10 +0800

    add js realize autoComplete

--- js/AutoComplete.js
+++ js/AutoComplete.js
@@ -3,8 +3,75 @@
  */
 var autoRead = ["apple", "orange", "banner", "Lemon", "Pear"];
 $(document).ready(function () {
-
     $("#input").autocomplete({
-        source: autoRead
+        source: autoRead,
+        change: function( event, ui ) {
+            console.log(ui);
+        }
     });
 });
+
+window.onload = function () {
+    var input_js = document.getElementById("input_js");
+    input_js.addEventListener("input", textChanged);
+}
+
+function textChanged() {
+    var input_js = document.getElementById("input_js");
+    var resultArr = returnCompleteArr(input_js.value);
+    var uls = createUl();
+    addLi(uls, resultArr);
+    document.getElementById("alabel").appendChild(uls);
+}
+
+function returnCompleteArr(str) {
+    var resultArr = [];
+    for(index in autoRead){
+        if(autoRead[index].indexOf(str) != -1){
+            resultArr.push(autoRead[index]);
+        }
+    }
+    return resultArr;
+}
+
+/* 创建<ul></ul> 并返回 */
+function createUl() {
+    if(judgemntExist("ulmain")){
+        var uls = document.createElement("ul");
+        uls.id = "ulmain";
+        uls.className = "ulJS";
+        return uls;
+    }
+    removeNodeAllChild(getElementID("ulmain"));
+    return getElementID("ulmain");
+}
+
+/* 创建<li></li>并添加到父节点中 */
+function addLi(uls, resultArr) {
+    for(index in resultArr){
+        var lis = document.createElement("li");
+        lis.innerHTML = resultArr[index];
+        uls.appendChild(lis);
+    }
+}
+
+/* 判断当前id是否在页面元素中 */
+function judgemntExist(strID) {
+    var uls = getElementID(strID);
+    if(typeof (uls) == null || typeof (uls) == undefined){
+        return false;
+    }
+    return true;
+}
+
+/* 删除该节点下的所有子节点 */
+function removeNodeAllChild(uls) {
+    for(var i = 0; i < uls.childNodes.length; i ++){
+        uls.removeChild(uls.childNodes[i]);
+    }
+}
+
+/* 根据id获取元素 */
+function getElementID(idStr) {
+    return document.getElementById(idStr);
+}
