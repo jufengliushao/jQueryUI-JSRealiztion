d4fc7efacb5627a5e5940922f28d39d144319de0
liushaofeng <jufengliushao@163.com>
2017-02-18 14:22:27 +0800

    add month year selector

--- js/DatePicker.js
+++ js/DatePicker.js
@@ -1,19 +1,24 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/18.
  */
-var showMonthBool = false;
+var showMonthBool = true;
+var showChangeBool = true;
 $(document).ready(function () {
     $("#date").datepicker({
         showOtherMonths: showMonthBool,
-        selectOtherMonth: showMonthBool
+        selectOtherMonth: showMonthBool,
+        showButtonPanel: true,
+        changeMonth: showChangeBool,
+        changeYear: showChangeBool
     });
-    $("#showMonth").button({
+    $("#showMonth, #change").button({
     });
 });
 
 window.onload = function () {
     getElementID("selecter").addEventListener("change", sliderValueChanged);
     getElementID("showMonth").addEventListener("click", showMonthAction);
+    getElementID("change").addEventListener("click", changeShow);
 };
 
 function sliderValueChanged() {
@@ -25,8 +30,23 @@ function showMonthAction() {
     var text = "显示月份选择按钮";
     if(showMonthBool){
         text = "隐藏月份选择按钮";
+    }else {
     }
     getElementID("showMonth").innerHTML = text;
+}
+
+function changeShow() {
+    showChangeBool = !showChangeBool;
+    var text = "显示月份年份选择按钮";
+    if (showChangeBool){
+        text = "隐藏月份年份选择按钮";
+    }
+    getElementID("change").innerHTML = text;
+    resetDatePicker();
+}
+
+function resetDatePicker() {
+
 }
 
 function getElementID(varID) {
