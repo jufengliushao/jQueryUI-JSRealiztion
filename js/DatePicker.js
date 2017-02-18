0143a8b89bcf2b0b822ac3cc35e130ca53adb534
liushaofeng <jufengliushao@163.com>
2017-02-18 13:50:49 +0800

    add datepicker animation

--- js/DatePicker.js
+++ js/DatePicker.js
@@ -1,3 +1,14 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/18.
  */
+$(document).ready(function () {
+    $("#date").datepicker();
+});
+
+window.onload = function () {
+    document.getElementById("selecter").addEventListener("change", sliderValueChanged);
+};
+
+function sliderValueChanged() {
+    console.log(this.value);
+}
