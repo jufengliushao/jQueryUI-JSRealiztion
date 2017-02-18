0744e6a96557c670b5b555618475ecdd1218f475
liushaofeng <jufengliushao@163.com>
2017-02-18 14:08:49 +0800

    complete show animation

--- js/DatePicker.js
+++ js/DatePicker.js
@@ -1,14 +1,34 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/18.
  */
+var showMonthBool = false;
 $(document).ready(function () {
-    $("#date").datepicker();
+    $("#date").datepicker({
+        showOtherMonths: showMonthBool,
+        selectOtherMonth: showMonthBool
+    });
+    $("#showMonth").button({
+    });
 });
 
 window.onload = function () {
-    document.getElementById("selecter").addEventListener("change", sliderValueChanged);
+    getElementID("selecter").addEventListener("change", sliderValueChanged);
+    getElementID("showMonth").addEventListener("click", showMonthAction);
 };
 
 function sliderValueChanged() {
-    console.log(this.value);
+    $("#date").datepicker("option", "showAnim", this.value);
+}
+
+function showMonthAction() {
+    showMonthBool = !showMonthBool;
+    var text = "显示月份选择按钮";
+    if(showMonthBool){
+        text = "隐藏月份选择按钮";
+    }
+    getElementID("showMonth").innerHTML = text;
+}
+
+function getElementID(varID) {
+    return document.getElementById(varID);
 }
