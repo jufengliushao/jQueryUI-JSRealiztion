72b19aa864064645a8c6288b5bbee1581744836c
liushaofeng <jufengliushao@163.com>
2017-02-19 22:32:06 +0800

    add dialog animation

--- js/Dailog.js
+++ js/Dailog.js
@@ -3,8 +3,19 @@
  */
 $(document).ready(function () {
     $("#showDig").button().on("click", showDailog);
+    $("#dailog_div").dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
 });
 
 function showDailog() {
-    $("#dailog_div").dialog();
+    $("#dailog_div").dialog("open");
 }
