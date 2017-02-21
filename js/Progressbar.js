15c60b1e870c457ecde66f16c0e9427f18b13539
liushaofeng <jufengliushao@163.com>
2017-02-21 21:28:36 +0800

    add progressbar value

--- js/Progressbar.js
+++ js/Progressbar.js
@@ -1,9 +1,29 @@
 /**
  * Created by cnlive-lsf-doc on 2017/2/21.
  */
+var current = 0;
 $(document).ready(function () {
     $("#progressbar").progressbar({
-        value: 30,
-        max: 100
+        value: current,
+        max: 100,
+        change: changeValue,
+        complete: valueComplete
     });
+    $("#btn").button();
+    $("#btn").on("click", addCurrent);
 });
+
+function changeValue() {
+    $(".progress-label").html($("#progressbar").progressbar("value")+"%");
+}
+
+function valueComplete() {
+    $(".progress-label").html("加载完成")
+}
+
+function addCurrent() {
+    current += 50;
+    $("#progressbar").progressbar({
+        value: current
+    });
+}
