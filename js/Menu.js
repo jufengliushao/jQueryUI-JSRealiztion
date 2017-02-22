37e29fbc372dad2e915ab574be6544ce1623b302
liushaofeng <jufengliushao@163.com>
2017-02-22 13:44:45 +0800

    add jquery ui image

--- js/Menu.js
+++ js/Menu.js
@@ -2,5 +2,7 @@
  * Created by cnlive-lsf-doc on 2017/2/22.
  */
 $(document).ready(function () {
-    $("#main_menu").menu();
+    $("#main_menu").menu({
+        icons: { submenu: "ui-icon-circle-triangle-e" }
+    });
 });
